d05658fa3c49da94fc9443019c14529609483e53
DESKTOP-DAP8HS0\Han <asdasdassb@gmail.com>
2022-11-13 16:25:48 +0800

    no message

--- lol_search/main.py
+++ lol_search/main.py
@@ -1,5 +1,18 @@
 from discord.ext import commands
+from cryptography.fernet import Fernet
+
+def get_token():
+    with open('cipher\secret_key.key') as f:
+        key = f.readline()
+
+    with open('cipher\cipher.txt') as f:
+        cipher = f.readline()
+
+    fernet = Fernet(key.encode())
+    dectex = fernet.decrypt(cipher.encode()).decode()
+
+    return dectex
 
 bot = commands.Bot(command_prefix='!')
 bot.load_extension("lol_search")
-bot.run("MTAzODMwODgzMTM0NTc4Mjg1NA.GOs2df.OCduPsYyilCkN7RHP0tzFtFeQxOOO2DNNcaNVI")
+bot.run(get_token())
